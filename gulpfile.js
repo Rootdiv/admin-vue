19dac5626ddbad6ea2411f345ba0b6b0be4fc5c3
Rootdiv <rootdiv@mail.ru>
2022-02-06 01:17:54 +0300

    feat: script for final build of the admin panel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 'use strict'
 
 const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 const browserify = require('browserify');
 const sass = require('gulp-sass')(require('sass'));
 
 const dist = '/home/maindiv/localhost/vue/admin';
+const distProd = './build'
 
 gulp.task('copy-html', () => gulp.src('./app/src/index.html').pipe(gulp.dest(dist)));
-gulp.task('copy-api', () => {
-  gulp.src('./app/api/**/*.*').pipe(gulp.dest(dist + '/api'));
-  gulp.src('./app/api/**/.*').pipe(gulp.dest(dist + '/api'));
-});
+
+gulp.task('copy-api', () => gulp.src('./app/api/**/*', { dot: true }).pipe(gulp.dest(dist + '/api')));
+
 gulp.task('copy-assets', () => gulp.src('./app/assets/**/*.*').pipe(gulp.dest(dist + '/assets')));
 
 gulp.task('build-js', () => browserify('./app/src/main.js', { debug: true }).transform('babelify', {
@@ -33,5 +37,19 @@ gulp.task('watch', () => {
 });
 
 gulp.task('build', gulp.parallel('copy-html', 'copy-api', 'copy-assets', 'build-js', 'build-sass'));
+
+gulp.task('build-prod', () => {
+  gulp.src('./app/src/index.html').pipe(gulp.dest(distProd));
+  gulp.src('./app/api/**/*', { dot: true }).pipe(gulp.dest(distProd + '/api'));
+  gulp.src('./app/assets/**/*.*').pipe(gulp.dest(distProd + '/assets'));
+  browserify('./app/src/main.js', { debug: false }).transform('babelify', {
+    presets: [
+      '@babel/preset-env',
+    ],
+    sourceMaps: false,
+  }).bundle().pipe(source('bundle.js')).pipe(buffer()).pipe(uglify()).pipe(gulp.dest(distProd));
+  return gulp.src('./app/scss/style.scss').pipe(sass().on('error', sass.logError)).pipe(autoprefixer())
+    .pipe(cleanCSS()).pipe(gulp.dest(distProd));
+})
 
 gulp.task('default', gulp.parallel('watch', 'build'));
